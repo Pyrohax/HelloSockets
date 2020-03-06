08d401d3d10e713c54f34b6584b567cdd09b71dc
JasperDre <jasperdre@gmail.com>
2020-03-07 00:30:16 +0100

    Update UnixServer.cpp

--- Server/Source/UnixServer.cpp
+++ Server/Source/UnixServer.cpp
@@ -15,27 +15,6 @@
 #define PORT "3490" // the port users will be connecting to
 #define BACKLOG 10 // how many pending connections queue will hold
 
-void sigchld_handler(int s)
-{
-	// waitpid() might overwrite errno, so we save and restore it:
-	int saved_errno = errno;
-
-	while (waitpid(-1, NULL, WNOHANG) > 0);
-
-	errno = saved_errno;
-}
-
-// get sockaddr, IPv4 or IPv6:
-void* get_in_addr(structsockaddr* sa)
-{
-	if (sa->sa_family == AF_INET)
-	{
-		return &(((structsockaddr_in*)sa)->sin_addr);
-	}
-
-	return &(((structsockaddr_in6*)sa)->sin6_addr);
-}
-
 int main()
 {
 	return 0;
